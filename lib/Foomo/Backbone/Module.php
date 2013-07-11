6ca4f5c8af61dcf091c9c31780203a05e5fbe89f
Jan Halfar <jan@bestbytes.com>
2013-07-11 12:40:12 +0200

    version management

--- lib/Foomo/Backbone/Module.php
+++ lib/Foomo/Backbone/Module.php
@@ -28,7 +28,7 @@ class Module extends \Foomo\Modules\ModuleBase
 	//---------------------------------------------------------------------------------------------
 	// ~ Constants
 	//---------------------------------------------------------------------------------------------
-
+	const VERSION = '0.2.0';
 	/**
 	 * the name of this module
 	 *
@@ -64,15 +64,7 @@ class Module extends \Foomo\Modules\ModuleBase
 	public static function getResources()
 	{
 		return array(
-			\Foomo\Modules\Resource\Module::getResource('Foomo.TypeScript', self::VERSION),
-			// get a run mode independent folder var/<runMode>/test
-			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'test'),
-			// and a file in it
-			// \Foomo\Modules\Resource\Fs::getVarResource(\Foomo\Modules\Resource\Fs::TYPE_File, 'test' . DIRECTORY_SEPARATOR . 'someFile'),
-			// request a cache resource
-			// \Foomo\Modules\Resource\Fs::getCacheResource(\Foomo\Modules\Resource\Fs::TYPE_FOLDER, 'navigationLeaves'),
-			// a database configuration
-			// \Foomo\Modules\Resource\Config::getResource('yourModule', 'db')
+			\Foomo\Modules\Resource\Module::getResource('Foomo.TypeScript', '0.1.*')
 		);
 	}
 }
