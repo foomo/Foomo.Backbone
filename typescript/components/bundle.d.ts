2d18a3a33fb2f691e727828c3988f00354399b76
ulrich <info@baltzer-consulting.de>
2014-10-13 11:01:17 +0200

    compiler update

--- typescript/components/bundle.d.ts
+++ typescript/components/bundle.d.ts
@@ -10,57 +10,57 @@ declare module Backbone {
     module Components {
         var mapToView: (view: View<Model>, mappings: Mapping[], verbose?: boolean) => {};
         class EventBinding {
-            public model: Model;
-            public event: string;
-            public handler: (model: Model, component: BaseComponent) => void;
+            model: Model;
+            event: string;
+            handler: (model: Model, component: BaseComponent) => void;
             constructor(model: Model, event: string, handler: (model: Model, component: BaseComponent) => void);
-            public toString(): string;
+            toString(): string;
         }
         class Mapping {
-            public selector: string;
-            public factory: (element: JQuery, view: View<Model>) => BaseComponent;
-            public eventBindings: EventBinding[];
-            public behaviours: any[];
-            public description: string;
+            selector: string;
+            factory: (element: JQuery, view: View<Model>) => BaseComponent;
+            eventBindings: EventBinding[];
+            behaviours: any[];
+            description: string;
             constructor(selector: string, factory: (element: JQuery, view: View<Model>) => BaseComponent, eventBindings: EventBinding[], behaviours?: any[], description?: string);
-            public toString(): string;
-            public addBehaviour(behaviourFactory: (component: BaseComponent) => Behaviour): Mapping;
+            toString(): string;
+            addBehaviour(behaviourFactory: (component: BaseComponent) => Behaviour): Mapping;
         }
         class Behaviour {
-            public component: BaseComponent;
+            component: BaseComponent;
             constructor(component: BaseComponent);
         }
         class BaseComponent extends View<Model> {
-            public view: View<Model>;
-            public id: string;
-            public attribute: string;
-            public bidirectionalBinding: boolean;
-            public behaviours: Behaviour[];
-            public whenData: {
+            view: View<Model>;
+            id: string;
+            attribute: string;
+            bidirectionalBinding: boolean;
+            behaviours: Behaviour[];
+            whenData: {
                 event: string;
                 model: Model;
             };
-            public toString(): string;
-            public getValue(): any;
-            public getOwnValue(): any;
-            public setValue(value: any): void;
-            public when(event: string, model?: Model): BaseComponent;
-            public then(handler: (model: Model, component: BaseComponent) => void): BaseComponent;
-            public attachBinding(binding: EventBinding): BaseComponent;
-            public bindModel(model: Model, attribute: string): void;
-            public handleChange(value: any): void;
-            public attachBehaviour(factory: (comp: BaseComponent) => Behaviour): void;
+            toString(): string;
+            getValue(): any;
+            getOwnValue(): any;
+            setValue(value: any): void;
+            when(event: string, model?: Model): BaseComponent;
+            then(handler: (model: Model, component: BaseComponent) => void): BaseComponent;
+            attachBinding(binding: EventBinding): BaseComponent;
+            bindModel(model: Model, attribute: string): void;
+            handleChange(value: any): void;
+            attachBehaviour(factory: (comp: BaseComponent) => Behaviour): void;
         }
         class Display extends BaseComponent {
-            public filter: (value: any) => string;
+            filter: (value: any) => string;
             static makeComp(): Display;
             static factory(element: JQuery, view: View<Model>, filter?: (value: any) => string): Display;
             static componentFactory(componentClass: {
                 makeComp: () => Display;
             }, element: JQuery, view: View<Model>, filter?: (value: any) => string): Display;
-            public setValue(value: string): void;
-            public getValue(): string;
-            public getOwnValue(): string;
+            setValue(value: string): void;
+            getValue(): string;
+            getOwnValue(): string;
             static map(selector: string): Mapping;
             static mapWithFilter(selector: string, filter: (value: any) => string): Mapping;
         }
@@ -69,35 +69,35 @@ declare module Backbone {
             static factory(element: JQuery, view: View<Model>, filter?: (value: any) => string): Display;
             static map(selector: string): Mapping;
             static mapWithFilter(selector: string, filter: (value: any) => string): Mapping;
-            public setValue(value: string): void;
-            public getValue(): string;
+            setValue(value: string): void;
+            getValue(): string;
         }
         class ListItemListener {
-            public event: string;
-            public handler: (item: View<Model>, event: any) => void;
-            public context: any;
+            event: string;
+            handler: (item: View<Model>, event: any) => void;
+            context: any;
             constructor(event: string, handler: (item: View<Model>, event: any) => void, context: any);
         }
         class List extends BaseComponent {
             private element;
             private viewClass;
             private itemListeners;
-            public itemViews: any[];
+            itemViews: any[];
             private ownValue;
             static factory(element: JQuery, view: View<Model>, viewClass: any, attribute?: string): List;
             static map(selector: string, viewClass: any, attribute?: string): Mapping;
-            public getValue(): any;
-            public getOwnValue(): any[];
-            public setValue(value: any[]): void;
-            public addListener(listener: ListItemListener): void;
+            getValue(): any;
+            getOwnValue(): any[];
+            setValue(value: any[]): void;
+            addListener(listener: ListItemListener): void;
         }
     }
 }
 declare module Backbone.Components {
     module Behaviours {
         class Feedback {
-            public message: string;
-            public level: string;
+            message: string;
+            level: string;
             static LEVEL_NONE: string;
             static LEVEL_OK: string;
             static LEVEL_INFO: string;
@@ -112,40 +112,40 @@ declare module Backbone.Components {
                 (): BaseValidator;
             }
             class Result {
-                public valid: boolean;
-                public message: string;
-                public level: string;
+                valid: boolean;
+                message: string;
+                level: string;
                 constructor(valid?: boolean, message?: string, level?: string);
             }
             class BaseValidator {
-                public validate(model: Model, attribute: string): Result;
+                validate(model: Model, attribute: string): Result;
             }
             var pack: (factory: IValidatorFactory, ...attributes: string[]) => any;
             class Package {
-                public attributes: string[];
-                public validatorFactory: IValidatorFactory;
+                attributes: string[];
+                validatorFactory: IValidatorFactory;
                 constructor(attributes: string[], validatorFactory: IValidatorFactory);
                 static make(factory: IValidatorFactory, ...attributes: string[]): Package;
             }
             class Validator {
-                public model: Model;
-                public feedbackModel: FeedbackModel;
+                model: Model;
+                feedbackModel: FeedbackModel;
                 constructor(model: Model, feedbackModel: FeedbackModel);
                 static create(model: Model, feedbackModel: FeedbackModel): Validator;
                 private chainAndMaybeAbort(abort, packages);
-                public chainAndAbortAfterFirstInvalid(...packages: Package[]): boolean;
-                public chain(...packages: Package[]): boolean;
+                chainAndAbortAfterFirstInvalid(...packages: Package[]): boolean;
+                chain(...packages: Package[]): boolean;
             }
         }
         class FeedbackModel extends Model {
             constructor(options?: any);
-            public clearFeedback(attribute: string): void;
-            public addFeedback(field: string, message: string, level?: string): void;
-            public getFeedback(field: string): Feedback[];
+            clearFeedback(attribute: string): void;
+            addFeedback(field: string, message: string, level?: string): void;
+            getFeedback(field: string): Feedback[];
         }
         class ComponentFeedback extends Behaviour {
-            public component: BaseComponent;
-            public feedbackModel: FeedbackModel;
+            component: BaseComponent;
+            feedbackModel: FeedbackModel;
             static FEEDBACK_CLASS: string;
             static FEEDBACK_TEXT_CLASS: string;
             private template;
@@ -155,7 +155,7 @@ declare module Backbone.Components {
             private loadFeedback();
         }
         class ListItemEventHandler extends Behaviour {
-            public component: List;
+            component: List;
             constructor(component: List, event: string, callback: (data: any) => void);
             static getFactory(event: string, callback: (data: any) => void): (component: List) => ListItemEventHandler;
         }
@@ -170,7 +170,7 @@ declare module Backbone.Components.Behaviours.Validation {
             };
             static pack(...attributes: any[]): any;
             static factory(): EmptyValidator;
-            public validate(model: Model, attribute: string): Result;
+            validate(model: Model, attribute: string): Result;
         }
         class LengthValidator extends BaseValidator {
             static MESSAGES: {
@@ -181,42 +181,43 @@ declare module Backbone.Components.Behaviours.Validation {
             private maxLength;
             static pack(minLength: number, maxLength: number, ...attributes: any[]): any;
             static factory(minLength: any, maxLength: any): LengthValidator;
-            public validate(model: Model, attribute: string): Result;
+            validate(model: Model, attribute: string): Result;
         }
     }
 }
 declare module Backbone.Components {
     module Controls {
         class Input extends BaseComponent {
-            public element: JQuery;
+            element: JQuery;
             static factory(element: JQuery, view: View<Model>): Input;
             static map(selector: any, bindings?: EventBinding[]): Mapping;
-            public setValue(value: any): void;
-            public getOwnValue(): any;
-            public getValue(): any;
+            setValue(value: any): void;
+            getOwnValue(): any;
+            getValue(): any;
         }
         class Checkbox extends Input {
-            public element: JQuery;
+            element: JQuery;
             private value;
-            public setValue(value: any): void;
-            public getOwnValue(): any;
-            public getValue(): any;
+            setValue(value: any): void;
+            getOwnValue(): any;
+            getValue(): any;
         }
         class Select extends BaseComponent {
-            public element: JQuery;
-            public options: any;
+            element: JQuery;
+            options: any;
             static factory(element: JQuery, view: View<Model>): Select;
             private static loadOptions(model, component, optionsAttribute);
             static map(selector: any, bindings?: EventBinding[]): Mapping;
             static mapWithOptionsFrom(selector: string, optionsAttribute: string, optionsModel: Model): Mapping;
-            public getOwnValue(): any;
-            public setValue(value: any): void;
-            public getValue(): any;
+            getOwnValue(): any;
+            setValue(value: any): void;
+            getValue(): any;
         }
         module Behaviours {
+            import Controls = Backbone.Components.Controls;
             class TypeToChange extends Behaviour {
-                constructor(component: Input);
-                static factory(component: Input): TypeToChange;
+                constructor(component: Controls.Input);
+                static factory(component: Controls.Input): TypeToChange;
             }
         }
     }
